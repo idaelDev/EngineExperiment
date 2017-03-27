25689fb5b54e7764fdfb1c928cdcde921110d853
Judicael <Judicael>
2017-03-27 16:55:14 +0200

    Add template for cpp files

--- WanderEngine/include/macros.h
+++ WanderEngine/include/macros.h
@@ -1,3 +1,18 @@
+/**********************************************************************
+//File			macros
+//Author		Judicael Abecassis
+//Last modified	27/03/2017 16:45:40
+//brief			Define all global macros
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**********************************************************************/
+
 #pragma once
 
 #if defined _WIN32 || __WINDOWS__
