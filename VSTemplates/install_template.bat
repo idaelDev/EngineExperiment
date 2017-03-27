65dd37c9fcd2271a2971a1c4c44f6f99f0b03eb8
IdaelDev <judicael.abecassis@gmail.com>
2017-03-27 19:30:12 +0200

    [FIX] install_template.bat for VS2017
    [ADD] Vector.cpp

--- VSTemplates/install_template.bat
+++ VSTemplates/install_template.bat
@@ -1,2 +1,2 @@
 xcopy ".\*.zip" "%UserProfile%\Documents\Visual Studio 2015\My Exported Templates\"
-xcopy ".\*.zip" "%UserProfile%\Documents\Visual Studio 2017\My Exported Templates\"
+xcopy ".\*.zip" "%UserProfile%\Documents\Visual Studio 2017\Templates\ItemTemplates\Projet Visual C++\"
---
+++ WanderEngine/src/Containers/Vector.cpp
@@ -0,0 +1,19 @@
+/**********************************************************************
+//File			Vector
+//Author		Judicael Abecassis
+//Last modified	27/03/2017 19:27:51
+//Definition	Vector.h
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**********************************************************************/
+
+#pragma region Includes
+#include "Containers/Vector.h"
+#pragma endregion
+
