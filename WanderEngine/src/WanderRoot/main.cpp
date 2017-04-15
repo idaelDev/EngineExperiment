b7b2b432becb629de93a362675fea1b1c79a10c8
jAbecassis <judicael.abecassis@gmail.com>
2017-04-15 02:08:09 +0200

    Complete rebuid and clean of the project folder architecture

---
+++ WanderEngine/src/WanderRoot/main.cpp
@@ -0,0 +1,20 @@
+/**********************************************************************
+//File			main
+//Author		Judicael Abecassis
+//Last modified	15/04/2017 01:50:40
+//Definition
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**********************************************************************/
+
+#pragma region Includes
+#pragma endregion
+
+void main()
+{}
